47d1ea068645688b7d8fd3c85b681a71d8464837
Marina Hristova <marina.ts.hristova@gmail.com>
2022-06-22 12:40:17 +0300

    Architecture update

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
+import { data, players, counter} from './data.js'
+
+
+
 const section = document.querySelector('section');
-let counter = 0;
-
-
-const data = [{ src: './Images/crocodile.jpg', name: 'crocodile' },
-{ src: './Images/elefant.jpg', name: 'elefant' },
-{ src: './Images/giraffe.webp', name: 'giraffe' },
-{ src: './Images/hippo.jpg', name: 'hippo' },
-{ src: './Images/lion.jpg', name: 'lion' },
-{ src: './Images/monkey.jpg', name: 'monkey' },
-{ src: './Images/tiger.webp', name: 'tiger' },
-{ src: './Images/zebra.png', name: 'zebra' },
-{ src: './Images/crocodile.jpg', name: 'crocodile' },
-{ src: './Images/elefant.jpg', name: 'elefant' },
-{ src: './Images/giraffe.webp', name: 'giraffe' },
-{ src: './Images/hippo.jpg', name: 'hippo' },
-{ src: './Images/lion.jpg', name: 'lion' },
-{ src: './Images/monkey.jpg', name: 'monkey' },
-{ src: './Images/tiger.webp', name: 'tiger' },
-{ src: './Images/zebra.png', name: 'zebra' }
-];
-
-
-const players=[];
 
 randomize(data);
 
---
+++ data.js
@@ -0,0 +1,22 @@
+export const data = [{ src: './Images/crocodile.jpg', name: 'crocodile' },
+{ src: './Images/elefant.jpg', name: 'elefant' },
+{ src: './Images/giraffe.webp', name: 'giraffe' },
+{ src: './Images/hippo.jpg', name: 'hippo' },
+{ src: './Images/lion.jpg', name: 'lion' },
+{ src: './Images/monkey.jpg', name: 'monkey' },
+{ src: './Images/tiger.webp', name: 'tiger' },
+{ src: './Images/zebra.png', name: 'zebra' },
+{ src: './Images/crocodile.jpg', name: 'crocodile' },
+{ src: './Images/elefant.jpg', name: 'elefant' },
+{ src: './Images/giraffe.webp', name: 'giraffe' },
+{ src: './Images/hippo.jpg', name: 'hippo' },
+{ src: './Images/lion.jpg', name: 'lion' },
+{ src: './Images/monkey.jpg', name: 'monkey' },
+{ src: './Images/tiger.webp', name: 'tiger' },
+{ src: './Images/zebra.png', name: 'zebra' }
+];
+
+
+export const players=[];
+
+export let counter = 0;
